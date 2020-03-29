95dc909bcf2a4530e599cdc8356fca1fa14f360e
Shehan Atuk <shehanatuk@gmail.com>
2020-03-29 11:12:50 -0400

    added base.css static file

--- fitness_forum/urls.py
+++ fitness_forum/urls.py
@@ -13,12 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns 
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView 
+from django.contrib.staticfiles import views 
 
+    
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('forum.urls')),
+    # re_path(r'^static/(?P<forum/styles>.css)$', views.serve)
     # path('forum/', include('forums.urls', namespace='forums-path'))
-]
+] + static(settings.STATIC_URL,
+document_root=settings.STATIC_ROOT)
+
+# # if settings.DEBUG:
+# #     urlpatterns += [
+# #         re_path(r'^static/(?P<path>.*)$', views.serve)
+#     ]
