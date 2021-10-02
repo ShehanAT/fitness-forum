4b392921bfdf266241b83a9d9e0ae1500fa05ee1
Nipun <shehanatuk@gmail.com>
2021-10-01 21:34:46 -0400

    added failing fixture register_new_user to test_auth.py

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -39,3 +39,26 @@ def test_register_fail_password_errors():
     assert response_content.find("The password is too similar to the username") != -1
     assert response_content.find("This password is too short. It must contain at least 8 characters") != -1
     assert response_content.find("This password is too common") != -1
+
+@pytest.fixture(autouse=True)
+def register_new_user():
+    rf = RequestFactory()
+    signup_url = reverse("forum:signup")
+    signup_request = rf.post(signup_url, {"username": "testUser", "email": "testUser@gmail.com", "password1": "Testuser123!", "password2": "Testuser123!"})
+    signup_response = signup_view(signup_request)
+    response_content = signup_response.content.decode("utf-8")
+    assert signup_response.status_code == 200 
+    test_login_success()
+    yield 
+
+@pytest.mark.django_db
+def test_login_success():
+    rf = RequestFactory()
+    login_url = reverse("forum:login")
+    login_request = rf.post(login_url, {"username": "testUser", "password": "Testuser123!"})
+    login_response = login_view(login_request)
+    response_content = login_response.content.decode("utf-8")
+    # print(response_content.find("Invalid Username/Password combiasdfa"))
+    # print(response_content.find("Invalid Username/Password combination"))
+    assert login_response.status_code == 200
+    assert response_content.find("Invalid Username/Password combination") == -1
