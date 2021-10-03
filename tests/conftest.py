d3f02a234c2638f46cacd69d49fad84bc50a44fa
Nipun <shehanatuk@gmail.com>
2021-10-02 21:46:00 -0400

    added fixture: registered_user() to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,8 @@ from pytest_factoryboy import register
 from rest_framework.test import APIClient, APIRequestFactory 
 from django.contrib.auth import get_user_model 
 from forum.models import ForumUser
+from django.contrib.auth.hashers import make_password
+from django import VERSION as DJANGO_VERSION
 
 @pytest.fixture
 def session():
@@ -22,6 +24,25 @@ def api_client():
 def api_rf():
     api_rf = APIRequestFactory()
     return api_rf 
+
+@pytest.fixture
+def registered_user():
+    user = ForumUserFactory(
+        username="admin",
+        email='admin@example.com', 
+        password=make_password('secret'), 
+        is_superuser=False,
+        first_name="admin",
+        last_name="admin",
+        is_active=True,
+        date_joined="2017-03-18 08:21:36.175627+07",
+        last_login="2017-03-20 08:21:36.175627+07"
+        )
+    registered_user = ForumUserFactory(user=user)
+    # assert registered_user.is_registered is True
+    # assert isinstance(registered_user.user, get_user_model())
+    assert isinstance(registered_user, ForumUser)
+    return registered_user
 
 
 @register
@@ -52,8 +73,8 @@ class ForumUserFactory(factory.django.DjangoModelFactory):
         forum_user = super(ForumUserFactory, cls).create(**kwargs)
         assert isinstance(forum_user, ForumUser)
         assert forum_user.is_authenticated is True 
-        assert forum_user.is_registered is True 
+        # assert forum_user.is_registered is True 
         return forum_user 
 
-    user = factory.SubFactory(UserFactory)
+    # user = factory.SubFactory(UserFactory)
 
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.test.client import RequestFactory
 from forum.views import login_view, signup_view
 
+
 @pytest.mark.django_db
 def test_login_fail():
     rf = RequestFactory()
@@ -40,25 +41,24 @@ def test_register_fail_password_errors():
     assert response_content.find("This password is too short. It must contain at least 8 characters") != -1
     assert response_content.find("This password is too common") != -1
 
-@pytest.fixture(autouse=True)
-def register_new_user():
-    rf = RequestFactory()
-    signup_url = reverse("forum:signup")
-    signup_request = rf.post(signup_url, {"username": "testUser", "email": "testUser@gmail.com", "password1": "Testuser123!", "password2": "Testuser123!"})
-    signup_response = signup_view(signup_request)
-    response_content = signup_response.content.decode("utf-8")
-    assert signup_response.status_code == 200 
-    test_login_success()
-    yield 
-
 @pytest.mark.django_db
-def test_login_success():
+def test_login_success(registered_user):
+    # TO-DO, status: Need to have registered_user populated with assigned values in registered_user() fixture
     rf = RequestFactory()
     login_url = reverse("forum:login")
-    login_request = rf.post(login_url, {"username": "testUser", "password": "Testuser123!"})
+    login_request = rf.post(login_url, 
+    {
+        "username": registered_user.username, 
+        "password": "secret"
+    })
     login_response = login_view(login_request)
     response_content = login_response.content.decode("utf-8")
+    print("username: " + registered_user.username)
+    print("email: " + registered_user.email)
+    print("password: " + registered_user.password)
     # print(response_content.find("Invalid Username/Password combiasdfa"))
     # print(response_content.find("Invalid Username/Password combination"))
     assert login_response.status_code == 200
-    assert response_content.find("Invalid Username/Password combination") == -1
+    # assert response_content.find("Invalid Username/Password combination") == -1
+
+
