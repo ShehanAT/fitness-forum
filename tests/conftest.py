2f3c2d8ac01ed2b95ae6720f2cff735491ccdc73
Nipun <shehanatuk@gmail.com>
2021-10-05 20:34:57 -0400

    added two new tests to test_routing.py, removed duplicate and redundant code from /tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,90 +6,22 @@ from django.contrib.auth import get_user_model
 from forum.models import ForumUser
 from django.contrib.auth.hashers import make_password
 from django import VERSION as DJANGO_VERSION
-
-@pytest.fixture
-def session():
-    engine = import_module(setting.SESSION_ENGINE)
-    session = engine.SessionStore()
-    session.create()
-    return session 
+from django.test import TestCase 
+from django.urls import reverse 
 
 
-@pytest.fixture 
-def api_client():
-    api_client = APIClient()
-    return api_client 
+class ConfTest(TestCase):
 
-@pytest.fixture
-def api_rf():
-    api_rf = APIRequestFactory()
-    return api_rf 
-
-# @pytest.fixture
-# def registered_user():
-#     user = ForumUserFactory(
-#         username="admin",
-#         email='admin@example.com', 
-#         password=make_password('secret'), 
-#         is_superuser=False,
-#         first_name="admin",
-#         last_name="admin",
-#         is_active=True,
-#         date_joined="2017-03-18 08:21:36.175627+07",
-#         last_login="2017-03-20 08:21:36.175627+07"
-#         )
-#     registered_user = ForumUserFactory(user=user)
-#     assert isinstance(registered_user, ForumUser)
-#     return registered_user
-
-@pytest.fixture
-def registered_forum_user():
-    registered_forum_user = ForumUserFactory(
-        username="admin",
-        email='admin@example.com', 
-        password=make_password('secret'), 
-        is_superuser=False,
-        first_name="admin",
-        last_name="admin",
-        is_active=True,
-        date_joined="2017-03-18 08:21:36.175627+07",
-        last_login="2017-03-20 08:21:36.175627+07"
-    )
-    # registered_forum_user = ForumUserFactory(user=user)
-    assert isinstance(registered_forum_user, ForumUser)
-    return registered_forum_user
-
-
-@register
-class UserFactory(factory.django.DjangoModelFactory):
-    class Meta: 
-        model = get_user_model()
-
-    @classmethod 
-    def create(cls, **kwargs):
-        user = super(UserFactory, cls).create(**kwargs)
-        assert isinstance(user, get_user_model())
-        if DJANGO_VERSION < (2,):
-            assert user.is_authenticated() is True 
-        else:
-            assert user.is_authenticated is True 
-        return user 
-    username = factory.Sequence(lambda n: 'uid-{}'.format(n))
-    password = 'secret'
-
-
-@register 
-class ForumUserFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = ForumUser 
-
-    @classmethod 
-    def create(cls, **kwargs):
-        forum_user = super(ForumUserFactory, cls).create(**kwargs)
-        assert isinstance(forum_user, ForumUser)
-        assert forum_user.is_authenticated is True 
-        # assert forum_user.is_registered is True 
-        return forum_user 
-
-    # user = factory.SubFactory(UserFactory)
+    @pytest.mark.django_db
+    def test_register_success(self):
+        register_url = reverse("forum:signup")
+        register_data = {
+            "username": "admin",
+            "email": "admin@gmail.com",
+            "password1": "Archimedes123",
+            "password2": "Archimedes123"
+        }
+        register_response = self.client.post(register_url, register_data)
+        assert register_response.status_code == 302
+        assert ForumUser.objects.count() > 0
 
---
+++ tests/test_models.py
@@ -0,0 +1,21 @@
+from django.contrib.sessions.backends.base import SessionBase
+import pytest
+from django.urls import reverse 
+from django.test.client import RequestFactory
+from forum.views import login_view, signup_view
+from django.conf import settings
+from django.contrib.sessions.middleware import SessionMiddleware
+from http.cookies import SimpleCookie
+import requests 
+from django.middleware import csrf
+from django.test import TestCase 
+from forum.models import ForumUser
+
+
+
+class TestAuthentication(TestCase):
+
+    @pytest.mark.django_db
+    def test_forum_user_(self):
+        forum_user = ForumUser();
+        
